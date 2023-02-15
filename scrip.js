70fa55ce6e407ed1ca3e74843cc0d690fd13ef81
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-02-15 18:46:29 -0300

    Requisito 17 e 18

--- scrip.js
+++ scrip.js
@@ -1,14 +1,20 @@
 const button = document.getElementById('button');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
+const btnEnviar = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
 
 const validaLogin = (event) => {
   event.preventDefault();
-    if (email.value === "tryber@teste.com" && senha.value === "123456") {
-      window.alert("Olá, Tryber!");
-    } else {
-      window.alert("Email ou senha inválidos.");
-    }
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
 };
 button.addEventListener('click', validaLogin);
 
+const habilite = () => {
+  btnEnviar.disabled = !checkbox.checked;
+};
+checkbox.addEventListener('change', habilite);
