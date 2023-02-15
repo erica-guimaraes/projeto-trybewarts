a91453775af3bee9b0e7064b8851e94b03cffa41
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-02-15 17:43:15 -0300

    requisitos 10 11 e 12

--- scrip.js
+++ scrip.js
@@ -1,13 +1,12 @@
 const button = document.getElementById('button');
 const email = document.getElementById('email');
-const senha =  document.getElementById('senha');
+const senha = document.getElementById('senha');
 
 const validaLogin = (event) => {
   event.preventDefault();
     if (email.value === "tryber@teste.com" && senha.value === "123456") {
       window.alert("Olá, Tryber!");
     } else {
-      
       window.alert("Email ou senha inválidos.");
     }
 };
